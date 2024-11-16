c70c4c1b421cf43ca92a8c36b14e04b19fd4fa31
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-16 12:10:15 +0000

    Update test case for potential stock API with new input value

--- backend/api_tests.py
+++ backend/api_tests.py
@@ -6,7 +6,7 @@ API_POST_URL = "https://fantastic-capybara-g4w9xx544wh5j4-5000.app.github.dev/"
 
 def test_get_potential_stock():
     """Test get_potential_stock"""
-    response = req.post(API_POST_URL + "stock", json={"iin": "834682"}, timeout=10)
+    response = req.post(API_POST_URL + "stock", json={"iin": "969431"}, timeout=10)
     # assert response.status_code == 200
     # print(response.json())
     # assert response.json() == ["Investment Mix 1", "Investment Mix 2"]
