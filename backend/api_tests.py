22c930b3f01759dfdb403b806cd5680bd8eaf47e
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-16 15:21:17 +0000

    Add test for retrieving user profile data and update stock test case

--- backend/api_tests.py
+++ backend/api_tests.py
@@ -6,10 +6,18 @@ API_POST_URL = "https://fantastic-capybara-g4w9xx544wh5j4-5000.app.github.dev/"
 
 def test_get_potential_stock():
     """Test get_potential_stock"""
-    response = req.post(API_POST_URL + "stock", json={"iin": "969431"}, timeout=10)
+    response = req.post(API_POST_URL + "stock", json={"iin": "216040"}, timeout=10)
     # assert response.status_code == 200
-    # print(response.json())
+    print(response.json())
     # assert response.json() == ["Investment Mix 1", "Investment Mix 2"]
 
+def test_get_profile_data():
+    """Test get_profile_data"""
+    response = req.post(API_POST_URL + "profile", timeout=10)
+    # assert response.status_code == 200
+    print(response.json())
+    # assert response.json()["full_name"] == "John Doe"
+
 if __name__ == "__main__":
-    test_get_potential_stock()
+    # test_get_potential_stock()
+    test_get_profile_data()
