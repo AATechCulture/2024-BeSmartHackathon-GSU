c6b9fd30292c16cb19639e908586657fcabdebb6
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-16 06:41:56 +0000

    Add timeout to bank data fetch request and create how-it-works documentation

--- backend/bank_gen.py
+++ backend/bank_gen.py
@@ -47,7 +47,7 @@ def fetch_data(iin: str) -> dict:
     """Fetches bank data"""
     # using requests to fetch the data of a bank iin
     response = req.post(
-       "https://fantastic-capybara-g4w9xx544wh5j4-5000.app.github.dev/bank", json={"iin": iin})
+       "https://fantastic-capybara-g4w9xx544wh5j4-5000.app.github.dev/bank", json={"iin": iin}, timeout=10)
     print(response)
     return response.json()
 
